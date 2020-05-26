55a2ba3e63a9643139680ad2db6c767873b42781
chidiebere onyedinma <codebreakersmd@gmail.com>
2020-05-26 13:55:28 -0400

    send node message

--- src/main/java/MPSSCore.java
+++ src/main/java/MPSSCore.java
@@ -5,7 +5,7 @@ import java.net.UnknownHostException;
 public class MPSSCore {
 
     public static void main(String[] args) throws UnknownHostException {
-        NetworkBus networkBus = new NetworkBus("localhost", 12345);
+        NetworkBus networkBus = new NetworkBus("192.168.1.85", 12345);
         networkBus.start();
     }
 
