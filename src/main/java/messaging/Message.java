a96559c08a6e10a31c978468a537ae24700884b4
chidiebere onyedinma <codebreakersmd@gmail.com>
2020-05-26 18:44:28 -0400

    send node message

--- src/main/java/messaging/Message.java
+++ src/main/java/messaging/Message.java
@@ -1,4 +1,21 @@
 package messaging;
 
+import org.json.JSONObject;
+
 public class Message {
+    private long timeStamp;
+    private JSONObject data;
+
+    public Message(JSONObject data, long timeStamp){
+        this.data = data;
+        this.timeStamp = timeStamp;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public JSONObject getData() {
+        return data;
+    }
 }
